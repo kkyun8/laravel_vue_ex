237f1fab05518c69b05ddaff2d2a6120be300cbd
kkyun8 <kim23302@icloud.com>
2020-07-08 00:21:43 +0900

    add dnd-grid

--- resources/js/components/SeatMaintenance/Layout.vue
+++ resources/js/components/SeatMaintenance/Layout.vue
@@ -1,14 +1,25 @@
 <template>
-  <div>Layout</div>
+  <div>
+    <h2>Layout</h2>
+    <Contaier>
+      <Box />
+    </Contaier>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
-// import { Container, Box } from "@dattn/dnd-grid";
-// import "@dattn/dnd-grid/dist/dnd-grid.css";
+//@ts-ignore
+import { Container, Box } from "@dattn/dnd-grid";
+import "@dattn/dnd-grid/dist/dnd-grid.css";
 
-@Component
+@Component({
+  components: {
+    Container,
+    Box
+  }
+})
 export default class Index extends Vue {
   message = "Laravel + Vue.js + TypeScript";
   mounted(): void {
