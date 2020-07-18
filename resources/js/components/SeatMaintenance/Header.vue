c1e19de3329d87ea4d8ad520c3ef3392728f949c
kkyun8 <kim23302@icloud.com>
2020-07-19 00:18:42 +0900

    席登録画面追加

--- resources/js/components/SeatMaintenance/Header.vue
+++ resources/js/components/SeatMaintenance/Header.vue
@@ -6,22 +6,113 @@
         <b-form-select label-field="Hall" v-model="hallId" :options="hallOptions"></b-form-select>
       </b-col>
       <b-col cols="6">
-        <b-form-select label-field="Hall" v-model="layoutId" :options="layoutOptions"></b-form-select>
+        <b-form-select label-field="Layout" v-model="layoutId" :options="layoutOptions"></b-form-select>
       </b-col>
     </b-row>
-    <!-- TODO: -->
-    <!-- <div>
-      <b-input-group class="mt-3">
-        <template v-slot:append>
-          <b-input-group-text>
-            <strong class="text-danger">!</strong>
-          </b-input-group-text>
+    <div class="card p-3 mb-2">
+      <!-- TODO: 配列？ -->
+      <b-row>
+        <b-form-group>
+          <h4>Insert Seat</h4>
+        </b-form-group>
+        <b-form-group
+          label-cols="6"
+          label="Seat Group Number Count"
+          label-for="seat-group-number-input"
+        >
+          <b-form-select
+            id="seat-group-number-input"
+            v-model="seatGroupNumber"
+            :options="seatGroupNumberOptions"
+          ></b-form-select>
+        </b-form-group>
+      </b-row>
+
+      <div class="form-row">
+        <template v-for="(n, nidx) in seatGroupNumber">
+          <b-form-group
+            class="col-md-2"
+            id="seat--number"
+            label="Seat Group Number"
+            :label-for="'seat-number-input' + nidx"
+            description="select number"
+            :key="'seat-number-' + nidx"
+          >
+            <b-form-input :id="'seat-group-number-input' + nidx" :value="nidx + 1" :disabled="true"></b-form-input>
+          </b-form-group>
+          <b-form-group
+            class="col-md-4"
+            :id="'seat-name-' + nidx"
+            description="set seat name."
+            label="Seat Name"
+            :label-for="'seat-name-input' + nidx"
+            :invalid-feedback="
+                            inputSeatName.length >= 4
+                                ? ''
+                                : 'Enter at least 4 characters'
+                        "
+            :key="'seat-name-' + nidx"
+            :state="inputSeatName.length >= 4 ? true : false"
+          >
+            <b-form-input
+              :id="'seat-name-input' + nidx"
+              v-model="inputSeatName"
+              :state="inputSeatName !== ''"
+              :type="'text'"
+              trim
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group
+            class="col-md-2"
+            id="seat-count"
+            label="Seat Head Count"
+            :label-for="'seat-count-input' + nidx"
+            :key="'seat-count-' + nidx"
+          >
+            <b-form-input
+              :id="'seat-count-input' + nidx"
+              v-model="inputSeatCount"
+              :type="'number'"
+              trim
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group
+            v-if="nidx === 0"
+            class="col-md-4"
+            id="seat-group-name"
+            description="set seat name."
+            label="Seat Group Name"
+            label-for="seat-group-name-input"
+            :invalid-feedback="
+                            nidx > 0
+                                ? inputSeatGroupName.length >= 4
+                                    ? ''
+                                    : 'Enter at least 4 characters'
+                                : ''
+                        "
+            :key="'seat-group-name-' + nidx"
+            :state="
+                            nidx > 0
+                                ? inputSeatGroupName.length >= 4
+                                    ? true
+                                    : false
+                                : true
+                        "
+            :disabled="nidx === 0"
+          >
+            <b-form-input
+              id="seat-group-name-input"
+              v-model="inputSeatGroupName"
+              :state="inputSeatGroupName !== ''"
+              :type="'text'"
+              trim
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group v-else :key="'seat-group-name-' + nidx" class="col-md-4"></b-form-group>
         </template>
-        <b-form-input></b-form-input>
-      </b-input-group>
-      <b-form-input></b-form-input>
-      <b-card></b-card>
-    </div>-->
+      </div>
+    </div>
+    <!-- TODO: -->
   </div>
 </template>
 
@@ -39,8 +130,8 @@ import mutations from "../../store/layout/mutations";
 @Component({})
 export default class Index extends Vue {
   @State("layout") layout!: LayoutState;
-  @Action("getHalls", { namespace }) getHalls: any;
-  @Action("getHallLayout", { namespace }) getHallLayout: any;
+  @Action("fetchHalls", { namespace }) fetchHalls: any;
+  @Action("fetchHallLayout", { namespace }) fetchHallLayout: any;
   @Mutation("setHallId", { namespace }) setHallId: any;
   @Mutation("setLayoutId", { namespace }) setLayoutId: any;
 
@@ -55,7 +146,7 @@ export default class Index extends Vue {
   }
   set layoutId(layoutId) {
     this.setLayoutId(layoutId);
-    this.$store.dispatch("layout/getHallLayout", layoutId);
+    this.$store.dispatch("layout/fetchHallLayout", layoutId);
   }
 
   get hallOptions() {
@@ -96,9 +187,23 @@ export default class Index extends Vue {
     return layoutOptions;
   }
 
+  //data
+  inputSeatName = "";
+  inputSeatCount = 1;
+  inputSeatGroupName = "";
+  seatGroupNumber = 1;
+  seatGroupNumberOptions = [
+    { value: 1, text: "1" },
+    { value: 2, text: "2" },
+    { value: 3, text: "3" },
+    { value: 4, text: "4" },
+    { value: 5, text: "5" },
+    { value: 6, text: "6" }
+  ];
+
   created() {
     // vue
-    this.$store.dispatch("layout/getHalls");
+    this.$store.dispatch("layout/fetchHalls");
   }
 
   mounted() {
