7bec2c8f2061fa1fa7df8ecb93387690e190f155
kkyun8 <kim23302@icloud.com>
2020-07-20 00:45:07 +0900

    typescriptエラー対応

--- resources/js/store/layout/mutations.ts
+++ resources/js/store/layout/mutations.ts
@@ -6,17 +6,17 @@ import { LayoutState, Layout } from "@/store/types";
 const mutations: MutationTree<LayoutState> = {
     setHalls: (state, halls: Layout["halls"]) => {
         state.halls = [];
-        halls.forEach(e => state.halls.push(e));
+        halls.forEach((e: any) => state.halls.push(e));
     },
     setHallLayout: (state, hallLayout: Layout["hallLayout"]) => {
         state.hallLayout = [];
-        hallLayout.forEach(e => {
+        hallLayout.forEach((e: any) => {
             state.hallLayout.push(e);
         });
     },
     setSeats: (state, seats: Layout["seats"]) => {
         state.seats = [];
-        seats.forEach(e => {
+        seats.forEach((e: any) => {
             const seat = new Seat(e);
             state.seats.push(seat);
         });
@@ -29,7 +29,7 @@ const mutations: MutationTree<LayoutState> = {
     },
     setEditSeats: (state, editSeats: Layout["editSeats"]) => {
         state.editSeats = [];
-        editSeats.forEach(e => {
+        editSeats.forEach((e: any) => {
             const seat = new Seat(e);
             state.editSeats.push(seat);
         });
