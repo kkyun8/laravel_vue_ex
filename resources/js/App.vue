bd66d090fdd6bfe1e5bd8b9459279876c471148a
kkyun8 <kim23302@icloud.com>
2020-07-20 00:44:39 +0900

    コンテナーclass修正

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,22 +1,22 @@
 <template>
-  <div>
-    <main class="m-1">
-      <b-container fluid class="p-1 m-1">
-        <RouterView />
-      </b-container>
-    </main>
-  </div>
+    <div>
+        <main class="m-1">
+            <b-container fluid>
+                <RouterView />
+            </b-container>
+        </main>
+    </div>
 </template>
 
 <script lang="ts">
 import { Vue } from "vue-property-decorator";
 export default class App extends Vue {
-  message = "Laravel + Vue.js + TypeScript";
+    message = "Laravel + Vue.js + TypeScript";
 }
 </script>
 <style scoped>
 h1 {
-  text-align: center;
-  margin: 4rem 0;
+    text-align: center;
+    margin: 4rem 0;
 }
 </style>
