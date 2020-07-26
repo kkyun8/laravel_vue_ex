8077330602b77163457c13d267514f219f72f388
kkyun8 <kim23302@icloud.com>
2020-07-27 00:40:21 +0900

    detail追加

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,14 +1,14 @@
 <template>
-    <div>
-        <notifications group="success" position="top left" :speed="300" />
-        <notifications group="warn" position="top left" :speed="300" />
-        <notifications group="error" position="top left" :speed="300" />
-        <main class="m-1">
-            <b-container fluid>
-                <RouterView />
-            </b-container>
-        </main>
-    </div>
+  <div>
+    <notifications group="success" position="top left" :speed="300" />
+    <notifications group="warn" position="top left" :speed="300" />
+    <notifications group="error" position="top left" :speed="300" />
+    <main class="m-1">
+      <b-container fluid>
+        <RouterView />
+      </b-container>
+    </main>
+  </div>
 </template>
 
 <script lang="ts">
@@ -20,80 +20,80 @@ import { LayoutState, Layout } from "./store/types";
 
 @Component
 export default class App extends Vue {
-    @State("layout") layout!: LayoutState;
-    @Mutation("setSuccess", { namespace }) setSuccess: any;
-    @Mutation("setWarning", { namespace }) setWarning: any;
-    @Mutation("setError", { namespace }) setError: any;
+  @State("layout") layout!: LayoutState;
+  @Mutation("setSuccess", { namespace }) setSuccess: any;
+  @Mutation("setWarning", { namespace }) setWarning: any;
+  @Mutation("setError", { namespace }) setError: any;
 
-    get success(): LayoutState["success"] {
-        return this.layout.success;
-    }
-    set success(success) {
-        this.setSuccess(success);
-    }
+  get success(): LayoutState["success"] {
+    return this.layout.success;
+  }
+  set success(success) {
+    this.setSuccess(success);
+  }
 
-    //TODO: warning message
-    get warning(): LayoutState["warning"] {
-        return this.layout.warning;
-    }
-    set warning(warning) {
-        this.setWarning(warning);
-    }
+  //TODO: warning message
+  get warning(): LayoutState["warning"] {
+    return this.layout.warning;
+  }
+  set warning(warning) {
+    this.setWarning(warning);
+  }
 
-    get error(): LayoutState["error"] {
-        return this.layout.error;
-    }
-    set error(error) {
-        this.setError(error);
-    }
+  get error(): LayoutState["error"] {
+    return this.layout.error;
+  }
+  set error(error) {
+    this.setError(error);
+  }
 
-    @Watch("success")
-    onSuccessChange(newVal: string, oldVal: string): string {
-        if (newVal !== "") {
-            this.show("success", "success", newVal);
-        }
-        return newVal;
+  @Watch("success")
+  onSuccessChange(newVal: string, oldVal: string): string {
+    if (newVal !== "") {
+      this.show("success", "success", newVal);
     }
+    return newVal;
+  }
 
-    @Watch("error")
-    onErrorChange(newVal: string, oldVal: string): string {
-        if (newVal !== "") {
-            this.show("error", "error", newVal);
-        }
-        return newVal;
+  @Watch("error")
+  onErrorChange(newVal: string, oldVal: string): string {
+    if (newVal !== "") {
+      this.show("error", "error", newVal);
     }
+    return newVal;
+  }
 
-    show(group: string, type = "", msg: string) {
-        const text = msg;
+  show(group: string, type = "", msg: string) {
+    const text = msg;
 
-        this.$notify({
-            group,
-            title: `${group}`,
-            text,
-            type,
-            data: {
-                randomNumber: Math.random()
-            }
-        });
+    this.$notify({
+      group,
+      title: `${group}`,
+      text,
+      type,
+      data: {
+        randomNumber: Math.random()
+      }
+    });
 
-        switch (group) {
-            case "success":
-                this.success = "";
-                break;
-            case "warning":
-                this.warning = "";
-                break;
-            case "error":
-                this.error = "";
-                break;
-            default:
-        }
+    switch (group) {
+      case "success":
+        this.success = "";
+        break;
+      case "warning":
+        this.warning = "";
+        break;
+      case "error":
+        this.error = "";
+        break;
+      default:
     }
+  }
 }
 </script>
 <style scoped>
 h1 {
-    text-align: center;
-    margin: 4rem 0;
+  text-align: center;
+  margin: 4rem 0;
 }
 </style>
--- resources/js/components/SeatMaintenance/SeatMaintenanceIndex.vue
+++ resources/js/components/SeatMaintenance/SeatMaintenanceIndex.vue
@@ -1,25 +1,51 @@
 <template>
-    <div>
-        <h2>Layout</h2>
-        <SeatMaintenanceHeader />
-        <SeatMaintenanceLayout />
-    </div>
+  <div>
+    <h2>Layout</h2>
+    <SeatMaintenanceHeader />
+    <SeatMaintenanceDetail @set_seats_data="setSeatsData" />
+    <SeatMaintenanceLayout :seats_data="seatsData" />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
 import SeatMaintenanceHeader from "@/components/seatMaintenance/SeatMaintenanceHeader.vue";
+import SeatMaintenanceDetail from "@/components/seatMaintenance/SeatMaintenanceDetail.vue";
 import SeatMaintenanceLayout from "@/components/seatMaintenance/SeatMaintenanceLayout.vue";
 
 @Component({
-    components: {
-        SeatMaintenanceHeader,
-        SeatMaintenanceLayout
-    }
+  components: {
+    SeatMaintenanceHeader,
+    SeatMaintenanceDetail,
+    SeatMaintenanceLayout
+  }
 })
 export default class SeatMaintenanceIndex extends Vue {
-    mounted(): void {}
+  seatsData: any[] = [];
+
+  setSeatsData(data: any[]) {
+    this.seatsData = [];
+    if (data.length > 0) {
+      data.forEach(e => this.seatsData.push(e));
+    }
+  }
+
+  created() {
+    // vue
+  }
+
+  mounted() {
+    // vue
+  }
+
+  updated() {
+    // vue
+  }
+
+  destroyed() {
+    // vue
+  }
 }
 </script>
 
