8a0a60f5293cdd984c7c5c2e5556ebf151a96ec5
kkyun8 <kim23302@icloud.com>
2020-07-30 00:34:40 +0900

    typeカラム追加

--- database/migrations/2020_07_05_165849_create_seats_table.php
+++ database/migrations/2020_07_05_165849_create_seats_table.php
@@ -18,11 +18,12 @@ class CreateSeatsTable extends Migration
       $table->bigIncrements('id');
       $table->integer('layout_id');
       $table->string('name')->unique();
-      $table->integer('seat_group_id')->nullable();;
+      $table->integer('seat_group_id')->nullable();
       $table->integer('w');
       $table->integer('h');
       $table->integer('x');
       $table->integer('y');
+      $table->integer('type')->default(0);
       $table->integer('count')->default(4);
       $table->boolean('delflg')->default(false);
       $table->timestamp('created_at')->useCurrent()->nullable();
