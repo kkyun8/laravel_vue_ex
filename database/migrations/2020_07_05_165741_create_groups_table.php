97996ab7228bede6e96afa7bc1d5bcfe3340442c
kkyun8 <kim23302@icloud.com>
2020-09-12 01:05:17 +0900

    gruops database update

--- database/migrations/2020_07_05_165741_create_groups_table.php
+++ database/migrations/2020_07_05_165741_create_groups_table.php
@@ -18,9 +18,11 @@ class CreateGroupsTable extends Migration
       $table->bigIncrements('id');
       $table->integer('group_code');
       $table->string('group_name');
+      $table->integer('group_number')->default(0);
       $table->timestamp('start_time');
       $table->timestamp('end_time');
       $table->integer('lock')->nullable();
+      $table->integer('layout_id')->nullable();
       $table->jsonb('seat')->nullable();
       $table->boolean('delflg')->default(false);
       $table->timestamp('created_at')->useCurrent()->nullable();
