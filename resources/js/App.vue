8cfcc2ae31374149eab12539f9601253621d528e
kkyun8 <kim23302@icloud.com>
2020-09-12 12:53:15 +0900

    nav 追加

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,14 +1,32 @@
 <template>
-  <div>
-    <notifications group="success" position="top left" :speed="300" />
-    <notifications group="warn" position="top left" :speed="300" />
-    <notifications group="error" position="top left" :speed="300" />
-    <main class="m-1">
-      <b-container fluid>
-        <RouterView />
-      </b-container>
-    </main>
-  </div>
+    <div>
+        <notifications group="success" position="top left" :speed="300" />
+        <notifications group="warn" position="top left" :speed="300" />
+        <notifications group="error" position="top left" :speed="300" />
+        <main class="m-1">
+            <b-container fluid>
+                <b-navbar variant="faded" type="light" class="">
+                    <b-navbar-brand href="/">Kkyun</b-navbar-brand>
+                    <b-collapse id="nav-collapse" is-nav>
+                        <b-navbar-nav>
+                            <b-nav-item
+                                href="/seating_chart"
+                                :disabled="setDisable('seating_chart')"
+                                >Seating chart</b-nav-item
+                            >
+                            <b-nav-item
+                                href="/seat_maintenance"
+                                :disabled="setDisable('seat_maintenance')"
+                                >Seat maintenance</b-nav-item
+                            >
+                        </b-navbar-nav>
+                    </b-collapse>
+                </b-navbar>
+                <div style="padding-top: 65px;" />
+                <RouterView />
+            </b-container>
+        </main>
+    </div>
 </template>
 
 <script lang="ts">
@@ -20,80 +38,84 @@ import { LayoutState, Layout } from "./store/types";
 
 @Component
 export default class App extends Vue {
-  @State("layout") layout!: LayoutState;
-  @Mutation("setSuccess", { namespace }) setSuccess: any;
-  @Mutation("setWarning", { namespace }) setWarning: any;
-  @Mutation("setError", { namespace }) setError: any;
+    @State("layout") layout!: LayoutState;
+    @Mutation("setSuccess", { namespace }) setSuccess: any;
+    @Mutation("setWarning", { namespace }) setWarning: any;
+    @Mutation("setError", { namespace }) setError: any;
 
-  get success(): LayoutState["success"] {
-    return this.layout.success;
-  }
-  set success(success) {
-    this.setSuccess(success);
-  }
-
-  //TODO: warning message
-  get warning(): LayoutState["warning"] {
-    return this.layout.warning;
-  }
-  set warning(warning) {
-    this.setWarning(warning);
-  }
-
-  get error(): LayoutState["error"] {
-    return this.layout.error;
-  }
-  set error(error) {
-    this.setError(error);
-  }
-
-  @Watch("success")
-  onSuccessChange(newVal: string, oldVal: string): string {
-    if (newVal !== "") {
-      this.show("success", "success", newVal);
+    get success(): LayoutState["success"] {
+        return this.layout.success;
     }
-    return newVal;
-  }
-
-  @Watch("error")
-  onErrorChange(newVal: string, oldVal: string): string {
-    if (newVal !== "") {
-      this.show("error", "error", newVal);
+    set success(success) {
+        this.setSuccess(success);
     }
-    return newVal;
-  }
 
-  show(group: string, type = "", msg: string) {
-    const text = msg;
-
-    this.$notify({
-      group,
-      title: `${group}`,
-      text,
-      type,
-      data: {
-        randomNumber: Math.random(),
-      },
-    });
-
-    switch (group) {
-      case "success":
-        this.success = "";
-        break;
-      case "warning":
-        this.warning = "";
-        break;
-      case "error":
-        this.error = "";
-        break;
-      default:
+    //TODO: warning message
+    get warning(): LayoutState["warning"] {
+        return this.layout.warning;
     }
-  }
+    set warning(warning) {
+        this.setWarning(warning);
+    }
+
+    get error(): LayoutState["error"] {
+        return this.layout.error;
+    }
+    set error(error) {
+        this.setError(error);
+    }
+
+    @Watch("success")
+    onSuccessChange(newVal: string, oldVal: string): string {
+        if (newVal !== "") {
+            this.show("success", "success", newVal);
+        }
+        return newVal;
+    }
+
+    @Watch("error")
+    onErrorChange(newVal: string, oldVal: string): string {
+        if (newVal !== "") {
+            this.show("error", "error", newVal);
+        }
+        return newVal;
+    }
+
+    show(group: string, type = "", msg: string) {
+        const text = msg;
+
+        this.$notify({
+            group,
+            title: `${group}`,
+            text,
+            type,
+            data: {
+                randomNumber: Math.random()
+            }
+        });
+
+        switch (group) {
+            case "success":
+                this.success = "";
+                break;
+            case "warning":
+                this.warning = "";
+                break;
+            case "error":
+                this.error = "";
+                break;
+            default:
+        }
+    }
+
+    setDisable(path: string) {
+        return this.$route.path.indexOf(path) >= 0;
+    }
 }
 </script>
 <style scoped>
 h1 {
-  text-align: center;
-  margin: 4rem 0;
+    text-align: center;
+    margin: 4rem 0;
 }
 </style>
